5dd49e3e58f268a281fd1809ef20a54108e436a0
pavelliber <pavel.liber@gmail.com>
2020-05-16 17:12:54 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+    render () {
+        return (
+            <div>
+                <header>Header</header>
+                <section>Search</section>
+                <section>Top News</section>
+                <section>Latest news</section>
+            </div>
+        )
+    }
+}
+
